b8c77524fa7e8e0b21533156c7f6ca275b1eba56
ThisDudeBoy <alex.moreira74@outlook.fr>
2021-04-17 20:41:22 +0200

    Fixed bug in stats command

--- commands/stats.js
+++ commands/stats.js
@@ -18,7 +18,8 @@ module.exports = {
     .setColor("#2f3136")
     .setTitle(`Informations about Joins+`)
     .setFooter(config.EmbedFooter)
-    .setDescription(`**__Informations__**:\n**Ram:** "+(process.memoryUsage().rss / 1024 / 1024).toFixed(2)+"MB\n**Uptime:** "+uptime.join(", ")+"\n**Shards:** 2\n**Ping:** "+Math.round(client.ws.ping)+"ms\n**Discord.js:** "+Discord.version+"\n**Node.js:** "+process.versions.node+"\n\n**__Statistics:__**\n**Servers:** " +client.guilds.cache.size+"\n**Members:** " +client.users.cache.size+"\n**Channels:** " +client.channels.cache.size+"\n**Emojis:** " +client.emojis.cache.size+"\n\n**__Links__**:\n**Support Server:** [Click me](https://discord.gg/RwYusY6)\n**Github:** [Click me](https://github.com/ThisDudeBoy/Join4Join)\n**Invite:** [Click me](https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=-1&scope=bot) `)
+    .setDescription(`**__Informations__**:\n**Ram:** `+(process.memoryUsage().rss / 1024 / 1024).toFixed(2)+`MB\n**Uptime:** `+uptime.join(", ")+`\n**Ping:** `+Math.round(client.ws.ping)+`ms\n**Discord.js:** `+Discord.version+`\n**Node.js:** `+process.versions.node+`\n\n**__Statistics:__**\n**Servers:** `+client.guilds.cache.size+`\n**Members:** ` +client.users.cache.size+`\n**Channels:** ` +client.channels.cache.size+`\n**Emojis:** ` +client.emojis.cache.size+`\n\n**__Links__**:\n**Support Server:** [Click me](https://discord.gg/RwYusY6)\n**Github:** [Click me](https://github.com/ThisDudeBoy/Join4Join)\n**Invite:** [Click me](https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=-1&scope=bot) `)
     message.channel.send(embed)
   }
+
 }
